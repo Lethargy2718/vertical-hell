c15ee614edf15149232d07f5d69dbcb6fff23cdb
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2026-02-21 04:28:28 +0200

    chore: remove redundant follow code from warning

--- Assets/Scripts/Warning.cs
+++ Assets/Scripts/Warning.cs
@@ -3,18 +3,7 @@ using UnityEngine;
 
 public class Warning : MonoBehaviour
 {
-    private FollowX _followX;
     public float duration = 1.0f;
-
-    private void Awake()
-    {
-        _followX = GetComponent<FollowX>();
-    }
-
-    public void Follow(Transform t)
-    {
-        _followX.followTarget = t;
-    }
 
     private void LateUpdate()
     {
