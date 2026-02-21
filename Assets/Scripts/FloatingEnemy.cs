73d13a4b04da9857cbd9677ef668f5e48291fdb2
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2026-02-21 22:52:08 +0200

    feat: add floating enemy with projectiles

---
+++ Assets/Scripts/FloatingEnemy.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using UnityEngine;
+
+public class FloatingEnemy : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform player;
+    private HealthComponent _healthComponent;
+
+    [Header("Positioning")]
+    [SerializeField] private float cameraOffsetY = 1f;
+    [SerializeField] private float playerOffsetX = 2f;
+
+    [Header("Movement")]
+    [SerializeField] private float acceleration = 2f;
+    [SerializeField] private float maxSpeed = 3f;
+    [SerializeField] private float drag = 0.95f;
+    [SerializeField] private float catchUpAcceleration = 6f;
+    [SerializeField] private float catchUpMaxSpeed = 7f;
+    [SerializeField] private float catchUpThreshold = 2f;
+    [SerializeField] private float catchUpResolvedThreshold = 0.5f;
+    [SerializeField] private float catchUpDuration = 2f;
+    private Vector2 _velocity;
+    private bool _isCatchingUp;
+    private float Acceleration => _isCatchingUp ? catchUpAcceleration : acceleration;
+    private float Speed => _isCatchingUp ? catchUpMaxSpeed : maxSpeed;
+    private float _startedCatchingUpTime = float.MinValue;
+
+    [Header("Projectiles")]
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private float shootProjectilesInterval = 5.0f;
+    private Coroutine _shootProjectilesRoutine;
+    private bool _isAttacking = false;
+
+    // Other
+    private LevelBounds LB => LevelBounds.Instance;
+    private float _time = 0f;
+
+    private void Awake()
+    {
+        _healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void OnEnable()
+    {
+        _healthComponent.HealthDepleted += Die;
+    }
+
+    private void OnDisable()
+    {
+        _healthComponent.HealthDepleted -= Die;
+    }
+
+    private void Start()
+    {
+        StartAttacking();
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float playerSide = player.position.x - LB.MidX;
+
+        float targetX = LB.MidX - playerSide + (playerSide < 0 ? playerOffsetX : -playerOffsetX);
+        float targetY = LB.CameraTopY - cameraOffsetY;
+        Vector2 target = new Vector2(targetX, targetY);
+
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        float distanceFromTarget = Vector2.Distance(transform.position, target);
+
+        float distanceFromTargetY = Mathf.Abs(targetY - transform.position.y);
+        float displacementFromPlayer = transform.position.y - player.position.y;
+        float displacementFromTargetY = transform.position.y - targetY;
+
+        bool belowPlayer = displacementFromPlayer < 0 && -displacementFromPlayer >= catchUpThreshold;
+        bool aboveTargetY = displacementFromTargetY > 0 && displacementFromTargetY >= catchUpThreshold;
+
+        if (!_isCatchingUp && (belowPlayer || aboveTargetY))
+        {
+            Debug.Log("Started catching up. Displacement from player: " + displacementFromPlayer);
+            StartCatchingUp();
+        }
+
+        bool catchUpDurationOver = _time >= _startedCatchingUpTime + catchUpDuration;
+        bool caughtUp = distanceFromTargetY <= catchUpResolvedThreshold;
+
+        if (_isCatchingUp && (caughtUp || catchUpDurationOver))
+        {
+            Debug.Log($"Finished catching up. Caught up: {caughtUp}. Duration over: {catchUpDurationOver}");
+            StopCatchingUp();
+        }
+
+        if (distanceFromTarget > 0.1f)
+            _velocity += Acceleration * Time.deltaTime * direction;
+
+        _velocity *= drag;
+        _velocity = Vector2.ClampMagnitude(_velocity, Speed);
+        transform.position += (Vector3)_velocity * Time.deltaTime;
+    }
+
+    private void StartCatchingUp()
+    {
+        _startedCatchingUpTime = _time;
+        _isCatchingUp = true;
+        StopAttacking();
+    }
+
+    private void StopCatchingUp()
+    {
+        _isCatchingUp = false;
+        StartAttacking();
+    }
+
+    private void StartAttacking()
+    {
+        if (_isAttacking) return;
+        _isAttacking = true;
+        _shootProjectilesRoutine = StartCoroutine(ShootProjectilesCoroutine());
+    }
+
+    private void StopAttacking()
+    {
+        if (!_isAttacking) return;
+        _isAttacking = false;
+        StopCoroutine(_shootProjectilesRoutine);
+        _shootProjectilesRoutine = null;
+    }
+
+    private IEnumerator ShootProjectilesCoroutine()
+    {
+        while (true)
+        {
+            ShootProjectile();
+            yield return new WaitForSeconds(shootProjectilesInterval);
+        }
+    }
+
+    private void ShootProjectile()
+    {
+        Projectile projectile = Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        projectile.Initialize(player.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Placeholder. Later, check collision with attack hitbox
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            _healthComponent.TakeDamage(25f);
+            StartCatchingUp();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float lifetime = 5f;
+
+    private Vector2 _direction;
+
+    public void Initialize(Vector2 targetPosition)
+    {
+        _direction = (targetPosition - (Vector2)transform.position).normalized;
+
+        float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + 90f);
+
+        Destroy(gameObject, lifetime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * _direction, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<HealthComponent>(out var healthComponent))
+        {
+            healthComponent.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
